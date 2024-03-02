eb390b28ca049d8c683a619e7d7fa8ff595ef035
emanuelfontoura <emanuelfontoura03@gmail.com>
2024-03-02 16:27:07 -0300

    começo dos estudos
    criação do workspace e do primeiro projeto

---
+++ primeiro_projeto/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int idade = 25;
+		double altura = 1.77;
+		char sexo = 'M';
+		
+		System.out.println("Meu nome é Emanuel, tenho " + idade + " anos, " + altura + " metros de altura e sou do sexo " + sexo + " (masculino).");
+	}
+
+}
